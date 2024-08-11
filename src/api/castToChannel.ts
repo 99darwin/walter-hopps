1167e06368165e395655c3feeace9c442db20ece
99darwin <nicksapsales@gmail.com>
2024-08-11 16:29:42 -0700

    feat: add channels, remove text from cast to channels

--- src/api/castToChannel.ts
+++ src/api/castToChannel.ts
@@ -5,19 +5,8 @@ import { randomUUID } from 'crypto';
 export async function castToChannel(
     channel: string,
     hash: string,
-    author: string,
     fid: number
 ) {
-    const messageOptions = [
-        `Congrats, @${author}, this piece has made the cut for our gallery. It captures the essence we\'re after—excited to see it on our walls.`,
-        `Good work, @${author}. This selection fits perfectly with the narrative we\'re building. Looking forward to unveiling it.`,
-        `Kudos, @${author}. This image will be a standout in our upcoming exhibition. It\'s exactly what we need to tie the collection together.`,
-        `Well done, @${author}. Your keen eye has added another gem to our gallery. This work will resonate deeply with our audience.`,
-        `Nice pick, @${author}. This image aligns beautifully with our vision. It\'s going to make quite the impact.`,
-        `Excellent choice, @${author}. This piece has that intangible quality we\'ve been searching for. Thrilled to see it in the gallery.`,
-        `Spot on, @${author}. This work adds just the right touch to our collection. Can't wait to see it displayed.`,
-        `Fantastic selection, @${author}. This image is a perfect fit for our show. It\'ll bring a fresh perspective to our narrative.`,
-    ];
     try {
         await axios({
             url: `${API_URL}/cast`,
@@ -34,12 +23,7 @@ export async function castToChannel(
                     },
                 ],
                 channel_id: channel,
-                text: messageOptions[
-                    Math.floor(Math.random() * messageOptions.length)
-                ], // Randomly select a message
                 signer_uuid: SIGNER_UUID,
-                // parent: hash,
-                // parent_author_fid: fid,
                 idem: randomUUID(),
             }),
         });
--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ const channels = [
   'https://warpcast.com/~/channel/art', 
   'https://warpcast.com/~/channel/ai-art',
   'https://warpcast.com/~/channel/cryptoart',
+  'https://warpcast.com/~/channel/superrare',
+  'https://warpcast.com/~/channel/foundation',
 ]
 
 // Validating necessary environment variables or configurations.
@@ -24,22 +26,11 @@ if (!NEYNAR_API_KEY) {
 }
 
 const replyToCast = async (castId: string, author: string, fid: number) => {
-  const messageOptions = [
-    `Congrats, @${author}, this piece has made the cut for our gallery. It captures the essence we\'re after—excited to see it on our walls. Check it out at /shelf.`,
-    `Good work, @${author}. This selection fits perfectly with the narrative we\'re building. Looking forward to unveiling it. Check it out at /shelf.`,
-    `Kudos, @${author}. This image will be a standout in our upcoming exhibition. It\'s exactly what we need to tie the collection together. Check it out at /shelf.`,
-    `Well done, @${author}. Your keen eye has added another gem to our gallery. This work will resonate deeply with our audience. Check it out at /shelf.`,
-    `Nice pick, @${author}. This image aligns beautifully with our vision. It\'s going to make quite the impact. Check it out at /shelf.`,
-    `Excellent choice, @${author}. This piece has that intangible quality we\'ve been searching for. Thrilled to see it in the gallery. Check it out at /shelf.`,
-    `Spot on, @${author}. This work adds just the right touch to our collection. Can't wait to see it displayed. Check it out at /shelf.`,
-    `Fantastic selection, @${author}. This image is a perfect fit for our show. It\'ll bring a fresh perspective to our narrative. Check it out at /shelf.`
-]
   try {
     await neynarClient.reactToCast(SIGNER_UUID, ReactionType.Like, castId);
     console.log(`Liked cast ${castId}`);
-    await castToChannel('shelf', castId, author, fid)
+    await castToChannel('shelf', castId, fid)
     console.log(`Recasted cast ${castId}`);
-    // await neynarClient.publishCast(SIGNER_UUID, messageOptions[Math.floor(Math.random() * messageOptions.length)], { replyTo: castId });
   } catch (err) {
     if (isApiErrorResponse(err)) {
       console.log(err.response.data);
